9d27bd4e8674844726a57ff1d1c73ec6709b5e4a
Ivan Pilot <ivanpilot@gmail.com>
2017-04-07 23:25:41 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,30 @@
+function postComment(){
+  var commenter = document.getElementById('commenterName').value;
+  var comment = document.getElementById('commentText').value;
+
+  // var commentTemplate = '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
+
+  var commentTemplate = document.getElementById('comment-template').innerHTML
+
+  var templateFn = _.template(commentTemplate);
+  var commentsDiv = document.getElementById('comments');
+
+  var templateHTML = templateFn({'commenter': commenter, 'comment': comment});
+
+  commentsDiv.innerHTML += templateHTML
+}
+
+// function postComment() {
+//   var commenter = document.getElementById("commenterName").value;
+//   var comment = document.getElementById("commentText").value;
+//
+//   var commentTemplate = '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
+//
+//   var templateFn = _.template(commentTemplate);
+//
+//   var commentsDiv = document.getElementById("comments");
+//
+//   var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
+//
+//   commentsDiv.innerHTML += templateHTML;
+// }
